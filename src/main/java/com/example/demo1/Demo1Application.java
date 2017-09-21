383316e8b4cae1c360d54b7898abc238bee35267
narisun <sundar.narisetti@gmail.com>
2017-09-21 10:44:49 -0400

    Update Demo1Application.java

--- src/main/java/com/example/demo1/Demo1Application.java
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -2,9 +2,21 @@ package com.example.demo1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
 public class Demo1Application {
+
+        @Override
+        public void run(String... args) throws Exception {
+		
+                int i=0;
+                for(;;)
+		{
+                        System.out.println("Application running ... count - "+(i++));
+                        Thread.sleep(1000);
+                }
+        }
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
